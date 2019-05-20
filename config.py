025264e4d639c5ef693d73c659741e29d4c0c6ac
Charles maina <muragemaina09@gmail.com>
2019-05-20 10:57:19 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
 
-   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey=7898c1e92e3f4963b15bf09d8dcb0c15'
-   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey=7898c1e92e3f4963b15bf09d8dcb0c15'
+   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey=99ee2ca0c9ad4113a5e47d61cd3728d2'
+   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey=99ee2ca0c9ad4113a5e47d61cd3728d2'
    	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
    	@staticmethod
    	def init_app(app):
